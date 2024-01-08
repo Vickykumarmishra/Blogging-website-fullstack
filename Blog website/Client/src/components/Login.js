7a4b0f9ab4c39592d237631e09f89ed06ea80437
vicky kumar mishra <mishravicky0141@gmail.com>
2024-01-09 00:10:27 +0530

    Update Login.js

--- Blog website/Client/src/components/Login.js
+++ Blog website/Client/src/components/Login.js
@@ -41,6 +41,29 @@ export default function Login() {
       }
       
       else{
+
+        let timerInterval;
+        Swal.fire({
+          title: "Auto close alert!",
+          html: "I will close in <b></b> milliseconds.",
+          timer: 80000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+              timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          }
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
         const response = await fetch('https://pranpratistha.onrender.com/login', {
           method: 'POST',
           headers: {
@@ -49,28 +72,7 @@ export default function Login() {
           body: JSON.stringify({ fullname, email, passw }),
         });
     
-        let timerInterval;
-Swal.fire({
-  title: "Auto close alert!",
-  html: "I will close in <b></b> milliseconds.",
-  timer: 80000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading();
-    const timer = Swal.getPopup().querySelector("b");
-    timerInterval = setInterval(() => {
-      timer.textContent = `${Swal.getTimerLeft()}`;
-    }, 100);
-  },
-  willClose: () => {
-    clearInterval(timerInterval);
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log("I was closed by the timer");
-  }
-});
+       
         if (response.ok) {
           const data = await response.json();
           localStorage.setItem('token', data.token);
